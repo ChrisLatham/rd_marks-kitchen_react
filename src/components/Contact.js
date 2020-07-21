87b3a7192d2277d367439b4767606ae3ba9bf2d8
ChrisLatham <chris@chrislatham.co.uk>
2020-07-21 19:53:14 +0100

    Modify table to correct syntax.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,34 +13,36 @@ const Contact = () => {
         <section className="panel opening-hours">
           <h2 className="first">Opening Hours</h2>
           <table className="table opening-hours-table">
-            <tr>
-              <td>Monday</td>
-              <td>17:00 - 23:00</td>
-            </tr>
-            <tr>
-              <td>Tuesday</td>
-              <td>Closed</td>
-            </tr>
-            <tr>
-              <td>Wednesday</td>
-              <td>17:00 - 23:00</td>
-            </tr>
-            <tr>
-              <td>Thursday</td>
-              <td>17:00 - 23:00</td>
-            </tr>
-            <tr>
-              <td>Friday</td>
-              <td>17:00 - 23:30</td>
-            </tr>
-            <tr>
-              <td>Saturday</td>
-              <td>17:00 - 23:30</td>
-            </tr>
-            <tr>
-              <td>Sunday</td>
-              <td>17:00 - 23:00</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Monday</td>
+                <td>17:00 - 23:00</td>
+              </tr>
+              <tr>
+                <td>Tuesday</td>
+                <td>Closed</td>
+              </tr>
+              <tr>
+                <td>Wednesday</td>
+                <td>17:00 - 23:00</td>
+              </tr>
+              <tr>
+                <td>Thursday</td>
+                <td>17:00 - 23:00</td>
+              </tr>
+              <tr>
+                <td>Friday</td>
+                <td>17:00 - 23:30</td>
+              </tr>
+              <tr>
+                <td>Saturday</td>
+                <td>17:00 - 23:30</td>
+              </tr>
+              <tr>
+                <td>Sunday</td>
+                <td>17:00 - 23:00</td>
+              </tr>
+            </tbody>
           </table>
           <div className="subtext">
             Christmas closed 25th 26th December. Open new years day.
